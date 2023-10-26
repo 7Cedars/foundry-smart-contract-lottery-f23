7606e31e16516870d288bc87b295f616d35e8d70
Seven Cedars <Cedars7@proton.me>
2023-10-26 16:00:33 +0100

    implmenting VRF2

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,28 +24,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import { VRFCoordinatorV2Interface } from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import { VRFConsumerBaseV2 } from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 /**
  * @title A sample Raffle Contract
  * @author Seven Cedars / Patrick Collins
  * @notice This contract was written during PC course on foundry and solidityu. 
  * @dev Implements Chainlink VRFv2
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
   error Raffle__NotEnoughEthSent(); 
+  error Raffle__TransferFailed(); 
+
+  /** Type declarations */
+  enum RaffleState {
+    OPEN,
+    CALCULATING
+  }
+
+  /** state variables */
+  uint16 private constant REQUEST_CONFIRMATIONS = 3; 
+  uint32 private constant NUM_WORDS = 1; 
   
   uint256 private immutable i_entranceFee; 
-  // @dev duration interval in seconds 
   uint256 private immutable i_interval;
+  VRFCoordinatorV2Interface private immutable i_vrfCoordinator; 
+  bytes32 private immutable i_gasLane; 
+  uint64  private immutable i_subscriptionId; 
+  uint32  private immutable i_callbackGasLimit; 
+
   address payable[] private s_players; 
+  address payable private s_recentWinner; 
   uint256 private s_lastTimeStamp;
   
   /** Events */
   event EnteredRaffle(address indexPlayer);  
 
-  constructor(uint256 entranceFee, uint256 interval) {
-    i_entranceFee = entranceFee; 
-    i_interval = interval; 
-    s_lastTimeStamp = block.timestamp; 
+  constructor(
+    uint256 entranceFee, 
+    uint256 interval, 
+    address vrfCoordinator, 
+    bytes32 gasLane, 
+    uint64 subscriptionId, 
+    uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+      i_entranceFee = entranceFee; 
+      i_interval = interval; 
+      i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator); 
+      i_gasLane = gasLane; 
+      i_subscriptionId = subscriptionId; 
+      i_callbackGasLimit = callbackGasLimit; 
+      s_lastTimeStamp = block.timestamp; 
+      
   }
 
   function enterRaffle() external payable { 
@@ -61,13 +92,33 @@ contract Raffle {
     if ((block.timestamp - s_lastTimeStamp) <= i_interval) {
       revert(); 
     }
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, 
+            i_callbackGasLimit,
+            NUM_WORDS    
+    );
   }
+
+  function fulfillRandomWords(
+    uint256 requestId, 
+    uint256[] memory randomWords
+  ) internal override {
+    uint256 indexOfWinner = randomWords[0] % s_players.length; 
+    address payable winner = s_players[indexOfWinner]; 
+    s_recentWinner = winner; 
+    (bool success, ) = winner.call{value: address(this).balance}(""); 
+    if (!success) {
+          revert Raffle__TransferFailed(); 
+    }
+  }
+          
 
   /** Getter Funcions */ 
   function getEntranceFee() external view returns (uint256) {
     return i_entranceFee; 
 
-
-  }
+  } 
 
 }
