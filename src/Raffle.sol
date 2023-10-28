300531be89d2bd722668d73d1fa7cfb6036e178e
Seven Cedars <Cedars7@proton.me>
2023-10-28 10:31:06 +0100

    written deploy scripts

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -130,7 +130,7 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     s_raffleState = RaffleState.CALCULATING; 
-    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+    i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS, 
